c8087eca8fb7e4a245e49d0f25aa4f3a425c88a0
Sudhanshu Verma <vermaji1372005@gmail.com>
2026-06-17 23:25:38 +0530

    usernameSet

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,13 +1,51 @@
 import "./app.css"
 import Editor from '@monaco-editor/react'
+import { MonacoBinding} from "y-monaco"
+import {useRef,useMemo,useState} from "react"
+import * as y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
+
+  const editorRef = useRef(null)
+  const {username,setUsername} = useState("")
+
+  const ydoc = useMemo(()=>new y.Doc(),[])
+  const yText = useMemo(()=>ydoc.getText("monaco"),[ydoc])
+
+  const handelMount = (editor)=>{
+    editorRef.current = editor
+    const provider = new SocketIOProvider("http://localhost:3000","monaco",ydoc,{autoConnect:true})
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+
+  }
+
+   
+
+  if(!username){
+    return(
+      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center">
+        <div className="flex flex-col gap-4">
+          <input type="text"
+          placeholder="Enter your username"
+          className="p-2 rounded-lg bg-grey-800 text-white"
+           />
+           <button className="p-2 rounded-lg bg-amber-50 text-grey-950 font-bold">join</button>
+        </div>
+      </main>
+    )
+  }
    
   return (
      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
       <aside className="h-full w-1/4  bg-amber-50 rounded-lg"></aside>
       <section className="w-3/4 bg-neutral-800 rounded-lg  overflow-hidden">
-      <Editor height="100%" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" />
+      <Editor height="100%" defaultLanguage="javascript" defaultValue="// some comment" theme="vs-dark" onMount={handelMount} />
       </section>
      </main>
   )
